0e587f19f29715e62e7f0441f9809ac990db7c56
pichai jodpimai <pichai.j@cpc.ac.th>
2024-09-10 16:42:26 +0700

    add SignIn and SignUp with MUI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import Login from './components/Login';
-import Register from './components/Register';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/login' element={<Login/>} />
-        <Route exact path='/register' element={<Register/>}/>
+        <Route exact path='/signin' element={<SignIn/>}/> 
+        <Route exact path='/signup' element={<SignUp/>}/>
+
       </Routes>
     </Router>
   );
